fe1d13566de3b758b3b605272a9eda060fbf85f0
GuilhermeGimenesTrue <guilherme.gimenes260609@gmail.com>
2025-09-25 12:48:39 -0300

    cpoommit

--- index.php
+++ index.php
@@ -1,9 +1,33 @@
 <?php 
     include 'pedaco.php';
 ?>
-        <p> </p>
-
-
+        <div>
+            <h1 class="titulo"> Como usar? <h1>
+            <p> Na navbar está localizado os menus de cadastro e atualização, no menu de cadastro, preencha cada campo corretamente, e por fim na lista está todos seus itens e o botão de atualizar e deletar.
+                Esse site está sendo utilizado como um gerenciador de inventário. Você pode colocar seus itens respectivamente com o preço e quantidade.
+            </p>
+        </div>
+        <section>
+            <div class="maintext">
+                <h2 class="titulos">
+                    O Que é um TTRPG
+                </h2>
+                <p>
+                    <img class="right" src="img/rpg.jpg" alt="rpg">
+                    "RPG é uma sigla em inglês que pode ser traduzida como “Jogo de Interpretação de Papéis” 
+                    ou “Jogo de Interpretação de Personagens”. Nele um grupo de amigos se reúne para construir uma história, 
+                    como se fosse um teatro de improviso. Existe um diretor, chamado de “narrador” ou “mestre”, 
+                    que vai explicando o desenrolar da trama; e existem os jogadores, que modificam a história à medida que interpretam seus personagens.
+                    Durante o jogo os personagens vivem aventuras que lembram os grandes épicos de nossa literatura e cinema: 
+                    enfrentam monstros, salvam princesas, desafiam impérios galácticos... Ou não, pois também é possível 
+                    interpretar vilões e anti-heróis. Existem RPGs de todos os tipos: de fantasia medieval ao terror, de viagens 
+                    espaciais a cenários históricos. E isso sem precisar desgrudar da cadeira e largar o refrigerante, pois tudo se passa na imaginação.
+                    Existem formas diferentes de jogar RPG e também outros jogos que derivaram dele, mas neste artigo trataremos unicamente do RPG 
+                    “de mesa”, o mais tradicional, onde os jogadores apenas dizem o que seus personagens farão, interpretando-os por meio de diálogos."
+                </p>
+            </div>
+        </section>
+        
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
     </main>
