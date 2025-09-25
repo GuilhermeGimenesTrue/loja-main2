bfcd972a78baa92cc387a3c6ae194ab84ab96900
GuilhermeGimenesTrue <guilherme.gimenes260609@gmail.com>
2025-09-25 09:07:33 -0300

    commiy

---
+++ pedaco.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LOJINHA ETECMCM</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+    <link href="css/meu-css.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Funnel+Display:wght@300..800&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+        <nav class="navbar bg-dark border-bottom border-body navmeu" data-bs-theme="dark">
+            <div class="container-fluid">
+                <img src="imagens/DNDBeyond_Logo.webp" alt="logo" class="logo">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="index.php">Inicio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="form_cadastra.php">Cadastrar</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="listar.php">Listar</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+    <main>  
+        <div class="container">
+            <h1>SISTEMA CRUD - LOJA</h1>
+            <h2>Guilherme Gimenes</h2>
+        </div>
